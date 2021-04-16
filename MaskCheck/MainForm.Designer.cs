9208797c31b447b0bd1e41ff41c9484f48d91779
Megageorgio <korogodoff@yandex.ru>
2021-04-16 15:44:43 +0400

    init

---
+++ MaskCheck/MainForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace MaskCheck
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.browseButton.Location = new System.Drawing.Point(15, 271);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(80, 28);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(15, 23);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 15);
+            this.resultLabel.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.browseButton);
+            this.panel1.Controls.Add(this.resultLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(441, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(107, 311);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.pictureBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(441, 311);
+            this.panel2.TabIndex = 3;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Image = global::MaskCheck.Properties.Resources.browse;
+            this.pictureBox.Location = new System.Drawing.Point(9, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(423, 287);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 311);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Mask Check";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ MaskCheck/MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaskCheckML.Model;
+
+namespace MaskCheck
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            using (var dlg = new OpenFileDialog()
+            {
+                Title = "Open Image",
+                Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png"
+            })
+            {
+                if (dlg.ShowDialog() is DialogResult.OK)
+                {
+                    browseButton.Enabled = false;
+                    ProcessImage(dlg.FileName);
+                }
+            }
+        }
+
+        private void ProcessImage(string path)
+        {
+            resultLabel.Text = "Wait...";
+            Task.Run(() => CheckMask(path));
+            pictureBox.Image = Image.FromFile(path);
+        }
+
+        private void CheckMask(string path)
+        {
+            var predictionResult = ConsumeModel.Predict(new ModelInput() {ImageSource = path});
+
+            if (resultLabel.InvokeRequired)
+            {
+                resultLabel.BeginInvoke((MethodInvoker) (() => SetResultLabel(predictionResult.Prediction)));
+            }
+            else
+            {
+                SetResultLabel(predictionResult.Prediction);
+            }
+        }
+
+        private void SetResultLabel(string prediction)
+        {
+            if (prediction is "with_mask")
+            {
+                resultLabel.ForeColor = Color.Lime;
+                resultLabel.Text = "With mask!";
+            }
+            else
+            {
+                resultLabel.ForeColor = Color.Red;
+                resultLabel.Text = "Without mask!";
+            }
+
+            browseButton.Enabled = true;
+        }
+    }
+}
